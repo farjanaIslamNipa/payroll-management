6a941d8202c93981b30fc5d31a45c4f8da604029
farjanaIslamNipa <nipa.farzana87@gmail.com>
2023-03-20 23:44:33 +0600

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="py-5 app-primary-bg app-primary-text">
+        <div class="container mx-auto pb-10 app-secondary-bg">
+            <div class="py-1 bg-gold capitalize">
+                <h3 class="text-xl font-bold text-center text-gray-700">salary information</h3>
+            </div>
+        <table class="min-w-full">
+          <thead class="">
+            <tr>
+              <th scope="col" class="text-sm font-bold px-6 py-2 text-left">
+                Month
+              </th>
+              <th scope="col" class="text-sm font-bold px-6 py-2 text-left">
+                Gross Salary
+              </th>
+              <th scope="col" class="text-sm font-bold px-6 py-2 text-left">
+                Basic
+              </th>
+              <th scope="col" class="text-sm font-bold px-6 py-2 text-left">
+                Home rent
+              </th>
+              <th scope="col" class="text-sm font-bold px-6 py-2 text-left">
+                Medical
+              </th>
+              <th scope="col" class="text-sm font-bold px-6 py-2 text-left">
+                Conveyance
+              </th>
+              <th scope="col" class="text-sm font-bold px-6 py-2 text-left">
+                Others
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="app-primary-bg border-b">
+              <td class="px-6 py-4 whitespace-nowrap text-sm">1</td>
+              <td class="text-sm font-light px-6 py-4 whitespace-nowrap">
+                Mark
+              </td>
+              <td class="text-sm font-light px-6 py-4 whitespace-nowrap">
+                Otto
+              </td>
+              <td class="text-sm font-light px-6 py-4 whitespace-nowrap">
+                @mdo
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
